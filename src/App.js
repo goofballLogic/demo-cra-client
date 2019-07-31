3ca8aa5c7008df1a563c26b131072b432db32d73
Andrew Gibson <andrew@goofballLogic.com>
2019-07-31 17:48:32 +0100

    abstracted out useAPIdata

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { AppStoreProvider } from "./AppStore";
 import Body from "./Body.js";
 import useStore from "./hooks/useStore";
-import config from "./config";
-import RestClient from './agents/RestClient';
+import useAPIdata from './hooks/useAPIdata';
 
-const postsClient = new RestClient({ root: config.api.posts });
-const commentsClient = new RestClient({ root: config.api.comments });
-const profileClient = new RestClient({ root: config.api.profile });
 const initialState = { posts: {}, comments: {}, profile: {} };
-
-const seedStore = (client, dispatch, actionPrefix) =>
-    client.getJSON()
-        .then(data => dispatch({ type: `${actionPrefix}.DATA`, data }))
-        .catch(err => dispatch({ type: `${actionPrefix}.ERROR`, err }));
 
 function App() {
 
   const [ store, dispatch ] = useStore( initialState );
-  useEffect(() => {
-
-    seedStore(postsClient, dispatch, "POSTS");
-    seedStore(commentsClient, dispatch, "COMMENTS");
-    seedStore(profileClient, dispatch, "PROFILE");
-
-  }, []);
+  useAPIdata( dispatch );
 
   return (
     <AppStoreProvider value={store}>
---
+++ src/hooks/useAPIdata.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import config from "../config";
+import RestClient from '../agents/RestClient';
+
+const postsClient = new RestClient({ root: config.api.posts });
+const commentsClient = new RestClient({ root: config.api.comments });
+const profileClient = new RestClient({ root: config.api.profile });
+
+async function seedStore(client, dispatch, actionPrefix) {
+
+    try {
+
+        const data = await client.getJSON()
+        dispatch({ type: `${actionPrefix}.DATA`, data });
+
+    } catch( err ) {
+
+        dispatch({ type: `${actionPrefix}.ERROR`, err });
+
+    }
+
+}
+
+function useAPIdata( dispatch ) {
+
+    useEffect(() => {
+
+        seedStore(postsClient, dispatch, "POSTS");
+        seedStore(commentsClient, dispatch, "COMMENTS");
+        seedStore(profileClient, dispatch, "PROFILE");
+
+    }, []);
+
+}
+
+export default useAPIdata;
